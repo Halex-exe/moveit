b163c9e9b60330dc94515425c0e7ae143bcd583a
Alexandre Klafke <alexandre.m.klafke@gmail.com>
2021-03-01 20:17:31 +0000

    Add stop countdown button

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/components/CompletedChallenges.module.css'
 export function CompletedChallenges() {
   return (
     <div className={styles.completedChallengesContainer}>
-      <span>Desafios completos</span>
+      <span>Complete challenges</span>
       <span>5</span>
     </div>
   )
